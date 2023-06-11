a72241ffd9109a99666ca02d1aae5e9846724f9c
Jakub Śliwka <jj_sliweczka@wp.pl>
2023-06-11 11:18:40 +0200

    update dnia 11.06.2023 skonczono liste 10 aisd laby

--- aisd_lab/Lista10/BST.java
+++ aisd_lab/Lista10/BST.java
@@ -1,6 +1,8 @@
 package Lista10;
-import java.util.ArrayList;
 
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.LinkedList;
 
 public class BST {
     class Node{
@@ -15,6 +17,9 @@ public class BST {
             linijki.add(linijka);
             leftChild = null;
             rightChild = null;
+        }
+        public ArrayList<Integer> getLinijki(){
+            return linijki;
         }
         public String getValue(){
             return value;
@@ -37,10 +42,87 @@ public class BST {
         public void setRightChild(Node node){
             rightChild = node;
         }
+        public void resetNode(String value, ArrayList<Integer> linijki){
+            this.value = value;
+            this.linijki = linijki;
+
+        }
     }
 
     Node root;
 
+    public void remove(String key){
+        root = remove(root, key);
+    }
+    
+    private Node remove(Node node, String key){
+        if(node == null){
+            return node;
+        }
+        else{
+            if(comparator(node.getValue(), key) == 1 ){
+                node.setLeftChild(remove(node.leftChild, key));
+            }
+
+            else if(comparator(node.getValue(), key) == -1){
+                node.setRightChild(remove(node.rightChild, key));                
+            }
+            
+            else{
+                if(node.getLeftChild() == null){
+                    return node.getRightChild();
+                }else if(node.getRightChild() == null){
+                    return node.getLeftChild();
+                }
+
+                // sa oba dzieci
+                //ustawienie nastepnika
+                //cos to nie dziala z 20
+                Node nastepnik = getNastepnik(node); 
+                node.resetNode(nastepnik.getValue(), nastepnik.getLinijki());
+
+                node.setRightChild(remove(node.getRightChild(), node.getValue()));
+                
+            }
+            
+            return node;
+        }
+    }
+
+    private Node getNastepnik(Node node){
+        if(node.getRightChild() != null){
+            Node tmpNode = node.getRightChild();
+            while(tmpNode.getLeftChild() != null){
+                tmpNode = tmpNode.getLeftChild();
+            }
+            return tmpNode;
+        }
+        return null;
+    }
+
+
+    public void wszerz(){
+        System.out.println("Przejscie drzewa wszerz:");
+        wszerz(root);
+        System.out.println();
+    }
+
+    private void wszerz(Node node){
+        Queue<Node> queue = new LinkedList<>();
+        if(node != null){
+            queue.add(node);
+            while(!queue.isEmpty()){
+                if(queue.peek().getLeftChild() != null){
+                    queue.add(queue.peek().getLeftChild());
+                }
+                if(queue.peek().getRightChild() != null){
+                    queue.add(queue.peek().getRightChild());
+                }
+                System.out.print(queue.remove().getValue()+" ");
+            }
+        }
+
+    }
     public void insert(String value, int linijka){
         if(root != null){
             if(containsWord(root, value)){
@@ -56,7 +138,7 @@ public class BST {
     }
 
     private void insert(Node node, Node father){
-        if(father.getValue().compareTo(node.getValue()) == 1){
+        if(comparator(father.getValue(), node.getValue()) == 1){
             if(father.getLeftChild() != null){
                 insert(node, father.getLeftChild());
             }
@@ -75,26 +157,77 @@ public class BST {
     }
 
     private boolean containsWord(Node node, String value){
-        if(node.getValue().compareTo(value) == 0){
+        if(comparator(value, node.getValue()) == 0){
             return true;
-        }else if(node.getLeftChild() != null && node.getLeftChild().getValue().compareTo(value) >= 0  ){
+        }else if(node.getLeftChild() != null && comparator(node.getValue(), value) == 1  ){
             return containsWord(node.getLeftChild(), value);
         }
-        else if(node.getRightChild() != null && node.getRightChild().getValue().compareTo(value) < 0 ){
+        else if(node.getRightChild() != null &&  comparator(node.getValue(), value) == -1 ){
             return containsWord(node.getRightChild(), value);
         }
         return false;
     }
     
     private void wyszukaIDodajLinijke(int linijka, String value, Node node){
-        if(node.getValue().compareTo(value) == 0){
+        if(comparator(value, node.getValue()) == 0){
             node.dodajLinijke(linijka);
         }
-        else if(node.getLeftChild() != null && node.getLeftChild().getValue().compareTo(value) >= 0 ){
+        else if(node.getLeftChild() != null && comparator(node.getValue(), value) == 1 ){
             wyszukaIDodajLinijke(linijka, value, node.getLeftChild());
         }
-        else if(node.getRightChild() != null && node.getRightChild().getValue().compareTo(value) <0 ){
+        else if(node.getRightChild() != null && comparator(node.getValue(), value) == -1 ){
             wyszukaIDodajLinijke(linijka, value, node.getRightChild());
         }
     }
+
+    public void inorder(){
+        System.out.println("Skorowidz:");
+        if(root != null){
+            inorder(root);
+        }
+    }
+
+    private void inorder(Node node){
+        if(node.getLeftChild() != null){
+            inorder(node.getLeftChild());
+        }
+        System.out.print(node.getValue());
+        for(int x: node.getLinijki()){
+            System.out.print(" " + x);
+        }
+        System.out.println();
+        if(node.getRightChild() != null){
+            inorder(node.getRightChild());
+        }
+    }
+
+    public static int comparator(String one, String two){
+        if(one.equals(two)){
+            return 0;
+        }
+        if(one.length() >= two.length()){
+            for(int x = 0; x < two.length(); x++){
+                if(one.charAt(x)>two.charAt(x)){
+                    return 1;
+                }
+                else if(one.charAt(x)<two.charAt(x)){
+                    return -1;
+                }
+            }
+            return 1;
+        }
+        if(two.length() > one.length()){
+            for(int x = 0; x < one.length(); x++){
+                if(one.charAt(x)>two.charAt(x)){
+                    return 1;
+                }
+                else if(one.charAt(x)<two.charAt(x)){
+                    return -1;
+                }
+            }
+            return -1;
+        }
+        return -1;
+    }
+
 }
--- aisd_lab/Lista10/tak.java
+++ aisd_lab/Lista10/tak.java
@@ -1,14 +1,63 @@
 package Lista10;
 
+import java.io.*; 
 public class tak {
-    public static void main(String[] args) {
-        BST bst = new BST();
-        System.out.println("chuj".compareTo("aasdaasdadasdsd"));
-        bst.insert("chuj", 1);
-        bst.insert("nulasdl", 10);
-        bst.insert("aasdasd", 12);
-        bst.insert("chuj", 122);
-        System.out.println("tak.main()");
+    private static BST bst;
+
+    public static int comparator(String one, String two){
+        if(one.equals(two)){
+            return 0;
+        }
+        if(one.length() >= two.length()){
+            for(int x = 0; x < two.length(); x++){
+                if(one.charAt(x)>two.charAt(x)){
+                    return 1;
+                }
+                else if(one.charAt(x)<two.charAt(x)){
+                    return -1;
+                }
+            }
+            return 1;
+        }
+        if(two.length() > one.length()){
+            for(int x = 0; x < one.length(); x++){
+                if(one.charAt(x)>two.charAt(x)){
+                    return 1;
+                }
+                else if(one.charAt(x)<two.charAt(x)){
+                    return -1;
+                }
+            }
+            return -1;
+        }
+        return -1;
+    }
+
+    public static void wczytajPlik() throws Exception{
+        File file = new File("tekst.txt");
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        
+        String line =  br.readLine();
+
+        bst = new BST();
+        int linijka = 1;
+        while(line != null){
+            for(String x:line.split("[,. ?!-_]+")){
+                bst.insert(x, linijka);
+            }
+            linijka +=1;
+            line = br.readLine();
+        }
+        System.out.println("tak.wczytajPlik()");
+    }   
+
+    public static void main(String[] args) throws Exception {
+        wczytajPlik();
+        bst.inorder();
+        bst.wszerz();
+        System.out.println(bst);
+        bst.remove("będą");
+        bst.wszerz();
     }
     
 }
